a39ca0394f53179f4f134ac5028c97a13537f9f2
Ryan1729 <Ryan1729@gmail.com>
2018-06-23 00:58:26 -0600

    2. try [3986284544, 1115046582, 4287666585, 1030160401]

--- src/common/inner_common.rs
+++ src/common/inner_common.rs
@@ -23,7 +23,7 @@ pub const PALETTE: [u32; 16] = [
     0xffaaccff,
 ];
 
-pub const TURTLE_COUNT: usize = 1;
+pub const TURTLE_COUNT: usize = 2;
 pub const INSTRUCTION_PAIR_COUNT: usize = 256 * 16;
 
 pub struct GameState {
